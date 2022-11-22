81cbefc895320dda3be4dd717f21b1c7b9beabbb
Antoine <anton.pukhart@gmail.com>
2022-11-22 20:32:26 +0300

    added schemas and documentation: with pyyaml, uritemplate and then drf-yasg

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,23 @@
 from django.urls import path, include 
 from django.contrib import admin
 # from rest_framework.schemas import get_schema_view
-# from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title='Blog API',
-#         default_version='v1',
-#         description='A sample API for learning DRF',
-#         terms_of_service='https://www.google.com/policies/terms/',
-#         contact=openapi.Contact(email='hello@example.com'),
-#         license=openapi.License(name='BSD License'),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Blog API',
+        default_version='v1',
+        description='A sample API for learning DRF',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='hello@example.com'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,8 +36,9 @@ urlpatterns = [
     #     description='A sample API for learning DRF',
     #     version='1.0.0'
     # ), name='openapi-schema'),
-    # path('swagger/', shcema_view.with_ui(
-    #     'swagger', cache_time=0), name='schema-swagger-ui'),
-    # path('redoc/', shcema_view.with_ui(
-    #     'redoc', cache_time=0), name='schema-redoc'),
+
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
